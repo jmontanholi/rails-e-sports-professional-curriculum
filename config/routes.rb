ee38f012194058457b963209131aba23c67f1eb3
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2022-01-18 16:14:14 -0300

    Add endpoints for companies

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
     post '/users/sign_in', to: 'users#signIn'
     post '/users/sign_up', to: 'users#signUp'
     delete '/users/delete', to: 'users#delete'
+
+    post '/companies/sign_in', to: 'companies#signIn'
+    post '/companies/sign_up', to: 'companies#signUp'
+    delete '/companies/delete', to: 'companies#delete'
   end
 end
